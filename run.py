f1bdcc9afefa8d77e01364569feb596b1055ee2c
Madelief04 <mschenkel@tudelft.nl>
2025-10-03 21:08:00 +0200

    added labels

--- run.py
+++ run.py
@@ -15,47 +15,71 @@ t_sine_slow = np.linspace(0, 4, len(sine_slow))
 t_sawtooth_fast = np.linspace(0, 1, len(sawtooth_fast))
 t_sawtooth_slow = np.linspace(0, 4, len(sawtooth_slow))
 
-#time arrays for plotting
+#plots
 plt.figure(figsize=(8, 4))
-
 plt.title('Original Sine Wave')
 plt.plot(t_sine, sine_wave)
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
 plt.show()
 
 plt.figure(figsize=(8, 4))
 plt.plot(t_sine_fast, sine_fast)
 plt.title('Fast Sine Wave')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
 plt.show()
 
 plt.figure(figsize=(8, 4))
 plt.plot(t_sine_slow, sine_slow)
 plt.title('Slow Sine Wave')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
 plt.show()
 
 plt.figure(figsize=(8, 4))
 plt.plot(t_sine, shifted_sine)
 plt.title('Shifted Sine Wave')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
 plt.show()
 
 plt.figure(figsize=(8, 4))
 plt.plot(t_sawtooth, sawtooth_wave)
 plt.title('Original Sawtooth Wave')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
 plt.show()
 
 plt.figure(figsize=(8, 4))
 plt.plot(t_sawtooth_fast, sawtooth_fast)
 plt.title('Fast Sawtooth Wave')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
 plt.show()
 
 plt.figure(figsize=(8, 4))
 plt.plot(t_sawtooth_slow, sawtooth_slow)
 plt.title('Slow Sawtooth Wave')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
 plt.show()
 
 plt.figure(figsize=(8, 4))
 plt.plot(t_sawtooth, shifted_sawtooth)
 plt.title('Shifted Sawtooth Wave')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid(True)
 plt.show()
+
 
 
 
