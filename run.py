73d3339f88e2f0df83ddee7f54556da2e9641705
Madelief04 <mschenkel@tudelft.nl>
2025-10-03 20:26:56 +0200

    change in format

--- run.py
+++ run.py
@@ -3,25 +3,55 @@ import numpy as np
 from scipy import signal
 
 
-def signals():
 
-    print("generating signals: ")
-    sine_wave = generate_sine_wave(2, 1, 2, 100)
-    sawtooth_wave = generate_sawtooth_wave(3, 0.4, 2, 100)
 
-    print("applying time shifting: ")
-    sine_shifted = time_shift_signal(sine_wave, 1, 100) 
-    sawtooth_shifted = time_shift_signal(sawtooth_wave, 0.5, 100)  
+#time arrays for plotting
+t_sine = np.linspace(0, 2, len(sine_wave))
+t_sawtooth = np.linspace(0, 2, len(sawtooth_wave))
 
-    print("applying time scaling:")
-    sine_fast = time_scale_signal(sine_wave, 2, 100)
-    sine_slow = time_scale_signal(sine_wave, 0.5, 100)
-    sawtooth_fast = time_scale_signal(sawtooth_wave, 2, 100)
-    sawtooth_slow = time_scale_signal(sawtooth_wave, 0.5, 100)
+#time arrays for scaled signals
+t_sine_fast = np.linspace(0, 1, len(sine_fast))  
+t_sine_slow = np.linspace(0, 4, len(sine_slow))  
+t_sawtooth_fast = np.linspace(0, 1, len(sawtooth_fast))
+t_sawtooth_slow = np.linspace(0, 4, len(sawtooth_slow))
 
-    # Create time arrays for plotting
-    t_original = np.linspace(0, 2, len(sine_wave))
-    t_fast = np.linspace(0, 1, len(sine_fast))  
-    t_slow = np.linspace(0, 4, len(sine_slow)) 
+plt.figure(figsize=(15, 12))
+
+plt.plot(t_sine, sine_wave)
+plt.plot(t_sine_fast, sine_fast)
+plt.plot(t_sine_slow, sine_slow)
+plt.plot(t_sine, sine_wave,)
+plt.plot(t_sine, shifted_sine)
+
+plt.plot(t_sawtooth, sawtooth_wave)
+plt.plot(t_sawtooth_fast, sawtooth_fast)
+plt.plot(t_sawtooth_slow, sawtooth_slow)
+plt.plot(t_sawtooth, sawtooth_wave)
+plt.plot(t_sawtooth, shifted_sawtooth)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
