91079036791f72856a47499d2e849264f5e2a295
Madelief04 <mschenkel@tudelft.nl>
2025-10-03 21:03:46 +0200

    made seperate plots

--- run.py
+++ run.py
@@ -15,19 +15,47 @@ t_sine_slow = np.linspace(0, 4, len(sine_slow))
 t_sawtooth_fast = np.linspace(0, 1, len(sawtooth_fast))
 t_sawtooth_slow = np.linspace(0, 4, len(sawtooth_slow))
 
-plt.figure(figsize=(15, 12))
+#time arrays for plotting
+plt.figure(figsize=(8, 4))
 
+plt.title('Original Sine Wave')
 plt.plot(t_sine, sine_wave)
-plt.plot(t_sine_fast, sine_fast)
-plt.plot(t_sine_slow, sine_slow)
-plt.plot(t_sine, sine_wave,)
-plt.plot(t_sine, shifted_sine)
+plt.show()
 
+plt.figure(figsize=(8, 4))
+plt.plot(t_sine_fast, sine_fast)
+plt.title('Fast Sine Wave')
+plt.show()
+
+plt.figure(figsize=(8, 4))
+plt.plot(t_sine_slow, sine_slow)
+plt.title('Slow Sine Wave')
+plt.show()
+
+plt.figure(figsize=(8, 4))
+plt.plot(t_sine, shifted_sine)
+plt.title('Shifted Sine Wave')
+plt.show()
+
+plt.figure(figsize=(8, 4))
 plt.plot(t_sawtooth, sawtooth_wave)
+plt.title('Original Sawtooth Wave')
+plt.show()
+
+plt.figure(figsize=(8, 4))
 plt.plot(t_sawtooth_fast, sawtooth_fast)
+plt.title('Fast Sawtooth Wave')
+plt.show()
+
+plt.figure(figsize=(8, 4))
 plt.plot(t_sawtooth_slow, sawtooth_slow)
-plt.plot(t_sawtooth, sawtooth_wave)
+plt.title('Slow Sawtooth Wave')
+plt.show()
+
+plt.figure(figsize=(8, 4))
 plt.plot(t_sawtooth, shifted_sawtooth)
+plt.title('Shifted Sawtooth Wave')
+plt.show()
 
 
 
