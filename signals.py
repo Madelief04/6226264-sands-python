1c45035106d46c679fb554d8cae28692965f2183
Madelief04 <mschenkel@tudelft.nl>
2025-10-03 14:26:20 +0200

    added a sawtooth singal with 'from scipy import signal'

--- signals.py
+++ signals.py
@@ -11,3 +11,12 @@ sine_wave = generate_sine_wave (2, 1, 2, 100)
 
 print (sine_wave)
 plt.plot(sine_wave)
+
+def generate_sawtooth_wave (frequency, amplitude, duration, sampling_rate, width =1 ): 
+    t = np.linspace(0, duration, int(sampling_rate * duration))
+    sawtooth_wave = amplitude * signal.sawtooth(2 * np.pi * frequency * t, width)
+    return sawtooth_wave
+
+sawtooth_wave = generate_sawtooth_wave (3, 0.4, 2, 100)
+
+plt.plot(sawtooth_wave)
