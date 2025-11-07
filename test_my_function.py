22acda89a5b9c0116b97206f8da6a0bdbcf5c5d1
Madelief04 <mschenkel@tudelft.nl>
2025-11-07 18:16:42 +0100

    one variable

--- test_my_function.py
+++ test_my_function.py
@@ -4,16 +4,16 @@ from signals import *
 def test_generate_sine_wave():
     """ Test the generate_sine_wave function with various test cases
     """
-    y, t = generate_sine_wave(2, 1, 2, 100)
+    y = generate_sine_wave(2, 1, 2, 100)
     assert len(t) == 200
 
-    y, t= generate_sine_wave(0, 1, 2, 100)
+    y = generate_sine_wave(0, 1, 2, 100)
     assert np.isclose(y, 0).all() 
 
-    y, t = generate_sine_wave(1, 2, 1, 1000)
+    y = generate_sine_wave(1, 2, 1, 1000)
     assert np.isclose(y[0], 0, atol=1e-6)      
 
-    y, t = generate_sine_wave(5, 3, 1, 1000)
+    y = generate_sine_wave(5, 3, 1, 1000)
     assert np.isclose(max(y), 3, atol=1e-6)
 
     print("it passed generate sine wave test \U0001F601!!!")
@@ -23,16 +23,16 @@ def test_generate_sawtooth_wave():
     """
     Test the generate_sawtooth_wave function with various test cases
     """
-    y, t = generate_sawtooth_wave(3, 0.4, 2, 100) 
+    y = generate_sawtooth_wave(3, 0.4, 2, 100) 
     assert len(t) == 200
 
-    y, t= generate_sawtooth_wave(0, 0.4, 2, 100)
+    y = generate_sawtooth_wave(0, 0.4, 2, 100)
     assert np.isclose(y, 0).all() 
 
-    y, t = generate_sawtooth_wave(1, 1, 1, 1000)
+    y = generate_sawtooth_wave(1, 1, 1, 1000)
     assert np.isclose(y[0], -1, atol=1e-6)     
 
-    y, t = generate_sawtooth_wave(5, 2, 1, 1000)
+    y = generate_sawtooth_wave(5, 2, 1, 1000)
     assert np.isclose(max(y), 2, atol=1e-6) 
 
     print("it passed generate saqtooth wave test \U0001F601!!") 
@@ -69,8 +69,6 @@ def test_time_scale_signal():
     assert len(scaled_signal) == 5
 
     print("it passed time scale test \U0001F601!!!")    
-
-
 
 
 
