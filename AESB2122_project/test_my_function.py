ddb67f3f918ab3786b6804861fb3b9583831d7f6
Madelief04 <mschenkel@tudelft.nl>
2025-11-07 21:44:29 +0100

    small change

--- AESB2122_project/test_my_function.py
+++ AESB2122_project/test_my_function.py
@@ -70,12 +70,6 @@ def test_time_scale_signal():
 
     print("it passed time scale test \U0001F601!!!")    
 
-if __name__ == "__main__":
-    test_generate_sine_wave()
-    test_generate_sawtooth_wave()
-    test_time_shift_signal()
-    test_time_scale_signal()
-    print("ALL TESTS PASSED! 😁!!!")
 
 
 
