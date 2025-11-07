d29c61f1f68f36acd74abee904b8e99e4b5734f7
Madelief04 <mschenkel@tudelft.nl>
2025-11-07 18:07:49 +0100

    ohhi hope it works  now

--- test_my_function.py
+++ test_my_function.py
@@ -71,13 +71,13 @@ def test_time_scale_signal():
     print("it passed time scale test \U0001F601!!!")    
 
 
-    if __name__ == "__main__":
-        test_generate_sine_wave()
-        test_generate_sawtooth_wave() 
-        test_time_scale_signal()
-        test_time_shift_signal()
-        print(" ALL TESTS PASSED \U0001F601 !!!")  
-
+if __name__ == "__main__":
+    print("🚀 Starting tests...")  # Add this line to see if main block runs
+    test_generate_sine_wave()
+    test_generate_sawtooth_wave() 
+    test_time_scale_signal()
+    test_time_shift_signal()
+    print("🎉 ALL TESTS PASSED 😁!!!")
 
 
 
